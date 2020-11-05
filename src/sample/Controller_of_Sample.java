55cd0672474dd0783275ed3f75eb9104766c3fd9
siraj6751 <siraj19176@iiitd.ac.in>
2020-11-05 12:45:07 +0530

    testing the functionality of exit and new game button

--- src/sample/Controller_of_Sample.java
+++ src/sample/Controller_of_Sample.java
@@ -1,17 +1,41 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
-public class Controller_of_Sample {
+import java.io.IOException;
+
+public class Controller_of_Sample extends Main {
     @FXML
     Pane pane1;
+
+    @FXML
+    Button b1, b2;
+
+    public void action1(ActionEvent e) throws IOException { // New Game button ka action
+        Parent root2 = FXMLLoader.load(getClass().getResource("sample1.fxml"));
+        Scene scene2=new Scene(root2, 300, 500);
+        scene2.setFill(Color.BLACK);
+        Main.getStage().setScene(scene2);
+
+    }
+
+    public void action2(ActionEvent e) throws IOException { // Exit game
+        System.exit(0);
+
+    }
+
 
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,21 +8,29 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static Stage guiStage;
 
-    @Override
+    public static Stage getStage() {
+        return guiStage;
+    }
+
+
     public void start(Stage primaryStage) throws Exception{
+        guiStage = primaryStage;
         Parent root1 = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Parent root2 = FXMLLoader.load(getClass().getResource("sample1.fxml"));
 
         Scene scene1=new Scene(root1, 395, 400);
         scene1.setFill(Color.BLACK);
-        Scene scene2=new Scene(root2, 300, 500);
 
+        Scene scene2=new Scene(root2, 300, 500);
         scene2.setFill(Color.BLACK);
         primaryStage.setTitle("Color Switch");
-        primaryStage.setScene(scene2);
+        primaryStage.setScene(scene1);
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
