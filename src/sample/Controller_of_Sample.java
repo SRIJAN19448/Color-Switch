a925558e72b597e6a3bf42b6a103f92f909cd23d
siraj6751 <siraj19176@iiitd.ac.in>
2020-11-05 11:37:58 +0530

    Update Controller_of_Sample.java

--- src/sample/Controller_of_Sample.java
+++ src/sample/Controller_of_Sample.java
@@ -1,5 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
 public class Controller_of_Sample {
 
 
