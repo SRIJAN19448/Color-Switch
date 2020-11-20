16a38422f7ee16e28dea44610d8f4f332fd492e6
siraj6751 <siraj19176@iiitd.ac.in>
2020-11-20 07:52:47 +0530

    hit_screen and controller of hit screen save_game_screen and controller of save game

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,15 +18,24 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         guiStage = primaryStage;
         Parent root1 = FXMLLoader.load(getClass().getResource("Main_Screen.fxml"));
-        Parent root2 = FXMLLoader.load(getClass().getResource("sample1.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("pause_screen.fxml"));
+        Parent root3 = FXMLLoader.load(getClass().getResource("hit_screen.fxml"));
+        Parent root4 = FXMLLoader.load(getClass().getResource("save_screen.fxml"));
 
         Scene scene1=new Scene(root1, 600, 600);
         scene1.setFill(Color.BLACK);
 
         Scene scene2=new Scene(root2, 300, 500);
         scene2.setFill(Color.BLACK);
+
+        Scene scene3=new Scene(root3, 300, 500);
+        scene3.setFill(Color.BLACK);
+
+        Scene scene4=new Scene(root4, 300, 500);
+        scene3.setFill(Color.BLACK);
+
         primaryStage.setTitle("Color Switch");
-        primaryStage.setScene(scene1);
+        primaryStage.setScene(scene4);
         primaryStage.show();
     }
 
