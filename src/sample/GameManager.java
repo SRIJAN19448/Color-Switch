eab9efa5ebe032dd759ce9e9ee8f594dd59ed2d1
SRIJAN19448 <srijan19448@iiitd.ac.in>
2020-11-20 14:01:49 +0530

    ss

--- src/sample/GameManager.java
+++ src/sample/GameManager.java
@@ -1,13 +1,14 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GameManager {
-    @FXML
-    Pane pn;
     Game game;
     public void new_game(Game g){
 
@@ -15,17 +16,19 @@ public class GameManager {
     public void load_game(Game g){
 
     }
-    public void start_game(Stage primaryStage){
-        Scene scene=new Scene(pn, 300, 500);
+    public void start_game(Stage primaryStage) throws Exception{
+        Parent root1 = FXMLLoader.load(getClass().getResource("Main_Screen.fxml"));
+        Scene scene1=new Scene(root1, 600, 600);
+        scene1.setFill(Color.BLACK);
         primaryStage.setTitle("Color Switch");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(scene1);
         primaryStage.show();
 
-        if(){
-            new_game(game);
-        }
-        else if(){
-            load_game(game);
-        }
+//        if(){
+//            new_game(game);
+//        }
+//        else if(){
+//            load_game(game);
+//        }
     }
 }
