5c61dc70570d85d09f7e03d5302287582d20f860
SRIJAN19448 <srijan19448@iiitd.ac.in>
2020-12-09 15:15:00 +0530

    added new

---
+++ src/sample/DoubleRingObs.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class DoubleRingObs implements Obstacle{
+    public DoubleRingObs(double pos,Ball ball,Game g){
+        super(ball,g,pos);
+
+    }
+
+    @Override
+    public void passThrough() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void detect_hit() throws InterruptedException {
+
+    }
+}
